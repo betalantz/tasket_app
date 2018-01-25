ce4b28203a0343bac758451ff7c11507b3eabbea
betalantz <betalantz@gmail.com>
2018-01-25 11:39:41 -0800

    try fix to manage.py settings module

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "d_appointment_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tasket_app.settings")
     
     # Different port manage.py file 
     import django
